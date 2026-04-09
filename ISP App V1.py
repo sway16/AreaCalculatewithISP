094cde609b8c0ef286a19d9c4b8abc6d783706d1
Dawsing marma <93827851+sway16@users.noreply.github.com>
2026-04-09 10:47:15 +0800

    Update ISP App V1.py

--- ISP App V1.py
+++ ISP App V1.py
@@ -1,17 +1,76 @@
-class AreaCalculator:
-    def calculate_area(self, shape, value):
-        if shape == "circle":
-            return 3.14 * value * value
-        elif shape == "square":
-            return value * value
-        elif shape == "rectangle":
-            return value[0] * value[1]
-        else:
-            return 0
+from abc import ABC, abstractmethod
 
+
+class AreaShape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class VolumeShape(ABC):
+    @abstractmethod
+    def volume(self):
+        pass
+
+
+class Circle(AreaShape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+
+class Square(AreaShape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+
+class Cube(AreaShape, VolumeShape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return 6 * self.side * self.side
+
+    def volume(self):
+        return self.side ** 3
+
+
+class AreaCalculator:
+    def calculate(self, shape: AreaShape):
+        return shape.area()
 
 calc = AreaCalculator()
 
-print(calc.calculate_area("circle", 5))
-print(calc.calculate_area("square", 4))
-print(calc.calculate_area("rectangle", (4, 6)))
+print("Circle Area:", calc.calculate(Circle(5)))
+print("Square Area:", calc.calculate(Square(4)))
+print("Cube Area:", calc.calculate(Cube(3)))
+
+
+cube = Cube(3)
+print("Cube Volume:", cube.volume())
+
+
+
+
+
+
+
+
+'''class Cylinder(AreaShape, VolumeShape):
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+
+    def area(self):
+        return 2 * 3.14 * self.radius * (self.height + self.radius)
+
+    def volume(self):
+        return 3.14 * self.radius**2 * self.height'''
+
+'''print("Cylinder Area:", Cylinder(3,5).area())
+print("Cylinder Volume:", Cylinder(3,5).volume())'''
